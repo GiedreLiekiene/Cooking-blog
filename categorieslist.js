688345a54c4a8fa62b20e875592ccc100dcef268
Giedre Liekiene <giedre.valanciute@gmail.com>
2022-09-02 16:04:23 +0300

    Updated cat.js

--- categorieslist.js
+++ categorieslist.js
@@ -6,7 +6,7 @@ function generateListElement(photo) {
         <li>
             <a
                 class="category-button"
-                href="https://unsplash.com/s/photos/vegan"
+                href="${photo.links.html}"
             >
                 <img src="${photo.urls.regular}" alt="" />
                 <span class="category-name">Vegan</span>
@@ -17,7 +17,7 @@ function generateListElement(photo) {
 
 function getCategories() {
   const count = 8;
-  const apiKey = `Y9lFDWqBGfIi7W2HpKfElFILUuo1C88SpqyuBoLc1TM`;
+  const apiKey = `l1IPdW9BJje5fHisAuviG9ctDmbtu-0BOZ8wXieAk1o`;
   let query = "food";
   let orientation = "squarish";
   const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orientation}&query=${query}`;
