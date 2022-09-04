40083c2e76ce494d704720d992187640706390e7
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-09-04 18:54:40 +0300

    new page - recipes.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,51 +1,12 @@
-
-// const mainGallery = document.getElementById("main-gallery");
-
-// let photoArray = [];
-// const count = 4;
-// const apiKey = `A9iQSfqWlY7erJtTYD5BSrKuZLlEBU5Pv_y3S4ZtVYU`;
-// let query = "food";
-// let orientation = "portrait";
-// const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&orientation=${orientation}&query=${query}`;
-
-// function setAttribute(element, atributes) {
-//   for (const key in atributes) {
-//     element.setAttribute(key, atributes[key]);
-//   }
-// }
-
-// function displayPhotos() {
-//   photoArray.forEach((photo) => {
-//     const item = document.createElement("a");
-//     item.setAttribute("href", photo.links.html);
-//     item.setAttribute("target", "_blank");
-//     const img = document.createElement("img");
-//     img.setAttribute("src", photo.urls.regular);
-//     img.setAttribute("alt", photo.alt_description);
-//     img.setAttribute("title", photo.alt_description);
-//     item.appendChild(img);
-//     mainGallery.appendChild(item);
-//   });
-// }
-
-// async function getPhotos() {
-//   try {
-//     const responce = await fetch(apiUrl);
-//     photoArray = await responce.json();
-//     displayPhotos();
-//   } catch (error) {}
-// }
-// getPhotos();
-
 //search-bar
 let search = document.querySelector(".search");
-console.log(search)
+console.log(search);
 let clear = document.querySelector(".clear");
 let input = document.getElementById("search");
 
 search.onclick = function () {
   document.querySelector(".search-container").classList.toggle("active");
-  getBack(input.innerHTML.length)
+  getBack(input.innerHTML.length);
 };
 
 clear.onclick = function () {
@@ -69,12 +30,35 @@ clear.ondblclick = function () {
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener('click', () => {
+hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
-})
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
-  hamburger.classList.toggle("active", false);
-  navMenu.classList.toggle("active", false);
-}));
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.toggle("active", false);
+    navMenu.classList.toggle("active", false);
+  })
+);
+
+// Carousel:
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
